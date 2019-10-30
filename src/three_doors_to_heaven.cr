5cc84e15e51d84349592deefbef1256832e85b54
takuya matsunaga <takanotume24@gmail.com>
2019-10-30 15:29:04 +0900

    最大日数表示を追加

--- src/three_doors_to_heaven.cr
+++ src/three_doors_to_heaven.cr
@@ -1,31 +1,40 @@
 def go_hell_1day(count : Int) : Int
-    return count+1
+  return count + 1
 end
+
 def go_hell_2day(count : Int) : Int
-    return count+2
+  return count + 2
 end
+
 def go_heaven(count : Int) : Int
-    return count
+  return count
 end
 
 def select_door(day : Int) : Int
-    door = Random.rand(3)
-    case(door)
-    when 0
-        return go_heaven day
-    when 1
-        return select_door(go_hell_1day day)
-    when 2
-        return select_door(go_hell_2day day)
-    end
-    return 0
+  door = Random.rand(3)
+  case (door)
+  when 0
+    return go_heaven day
+  when 1
+    return select_door(go_hell_1day day)
+  when 2
+    return select_door(go_hell_2day day)
+  end
+  return 0
 end
 
-result : Int64= 0 
+result = 0
+sum = 0
+max = 0
 loop = ARGV[0].to_i64
 
 loop.times do |i|
-    result += select_door(0_u64)
+  result = select_door(0_u64)
+  sum += result
+  if max < result
+    max = result
+  end
 end
 
-print result / loop.to_f
+puts "天国に行ける平均日数: #{sum / loop.to_f}日"
+puts "最長で地獄にいた日数: #{max}日"
